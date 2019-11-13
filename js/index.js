f95094b266ae33cb62c05c832d534262f6d7424e
dxshim90 <dxshim90@gmail.com>
2019-11-13 10:44:47 +0000

    Created seperate functions to create and append shuffle show/hide and magice buttons to the dom

--- js/index.js
+++ js/index.js
@@ -1,5 +1,6 @@
 const suit = 'hearts';
 const cardsWrapper = document.querySelector('.cards-wrapper');
+const buttonsWrapper = document.querySelector('.btn-wrapper')
 
 function createCards() {
   const cards = [];
@@ -23,9 +24,39 @@ function createCards() {
   });
 }
 
+const createShuffleButton = () => {
+  const shuffleButton = document.createElement('button')
+  shuffleButton.setAttribute('id', 'shuffle-btn')
+  shuffleButton.innerHTML = "Shuffle"
+  shuffleButton.className = 'btn btn-lg btn-secondary'
+  shuffleButton.style.margin = '1rem'
+  buttonsWrapper.appendChild(shuffleButton)
+}
+
+const createShowHideButton = () => {
+  const showHideButton = document.createElement('button')
+  showHideButton.setAttribute('id', 'showhide-btn')
+  showHideButton.innerHTML = "Show/Hide"
+  showHideButton.className = 'btn btn-lg btn-secondary'
+  showHideButton.style.margin = '1rem'
+  buttonsWrapper.appendChild(showHideButton)
+}
+
+const createMagicButton = () => {
+  const magicButton = document.createElement('button')
+  magicButton.setAttribute('id', 'magicbtn')
+  magicButton.innerHTML = "Magic"
+  magicButton.className = 'btn btn-lg btn-secondary'
+  magicButton.style.margin = '1rem'
+  buttonsWrapper.appendChild(magicButton)
+}
+
 // Function to clear out the initial button and create new buttons to play the game.
 function createButtons() {
-  // Your Code
+  document.getElementById('start-game').remove()
+  createShuffleButton()
+  createShowHideButton()
+  createMagicButton()
 }
 
 // Function to start the game by clearing the wrapper, creating
